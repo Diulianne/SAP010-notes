8ddcc0c039682ea40ce3ccd66e263d326ce26e84
Diulianne Oliveira <diulicarine@gmail.com>
2023-09-04 10:40:59 -0400

    fiz o css da pag de cadastro

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -1,51 +1,8 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { signInWithEmailAndPassword, updateProfile, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from 'react-router-dom';
-
-
-// const SignIn = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigate = useNavigate();
-//   const handleSignIn = async () => {
-//     try {
-//       // login do usuário com email e senha
-//       const userCredential = await signInWithEmailAndPassword(auth, email, password);
-
-//       // Se o login for bem-sucedido, você pode acessar o usuário assim:
-//       const user = userCredential.user;
-//       console.log("Usuário logado com sucesso: ", user);
-      
-//       navigate('/home');
-//     } catch (error) {
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       console.error("Erro ao fazer login: ", errorCode, errorMessage);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Login</h1>
-//       <input
-//         type="email"
-//         placeholder="Email"
-//         value={email}
-//         onChange={(e) => setEmail(e.target.value)}
-//       />
-//       <input
-//         type="password"
-//         placeholder="Senha"
-//         value={password}
-//         onChange={(e) => setPassword(e.target.value)}
-//       />
-//       <button onClick={handleSignIn}>Entrar</button>
-//     </div>
-//   );
-// };
-
-// export default SignIn;
+import "../login/login.css"
 
 
 const SignUp = () => {
@@ -56,7 +13,7 @@ const SignUp = () => {
 
   const handleSignUp = async () => {
     try {
-      // Crie o usuário com email e senha
+      // Cria o usuário com email e senha
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       await updateProfile(userCredential.user, { displayName: name });
       await signInWithEmailAndPassword(auth, email, password);
@@ -70,8 +27,9 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h1>Cadastro</h1>
+    <div className="login-container">
+      <img className="logo" src="logo.png" alt="Logo" />
+      <h2 className="title">Sign Up LabNotes</h2>
       <input
         type="text"
         placeholder="Nome"
@@ -90,7 +48,10 @@ const SignUp = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleSignUp}>Criar Conta</button>
+      <button className="login-button" onClick={handleSignUp}>Criar Conta</button>
+      <p className="create-account">
+      Já possui uma conta? <span onClick={() => navigate('/login')}>Login</span>
+      </p>
     </div>
   );
 };
